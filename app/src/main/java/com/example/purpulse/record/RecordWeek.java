474a16b6a5d1a471edf15266f8700c4f5662a458
ei8ht <yitin0826@gmail.com>
2022-11-09 23:28:11 +0800

    Record大改

--- app/src/main/java/com/example/purpulse/record/RecordWeek.java
+++ app/src/main/java/com/example/purpulse/record/RecordWeek.java
@@ -1,16 +1,78 @@
 package com.example.purpulse.record;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.purpulse.Note;
 import com.example.purpulse.R;
+import com.example.purpulse.SqlDataBaseHelper;
+import com.example.purpulse.result.ResultPagerAdapter;
+import com.example.purpulse.result.ScatterFragment;
+import com.example.purpulse.result.TaijiFragment;
+import com.github.mikephil.charting.charts.LineChart;
+import com.google.android.material.tabs.TabLayout;
+
+import org.angmarch.views.NiceSpinner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 
 public class RecordWeek extends Fragment {
+
+    /** UI **/
+    View view;
+    private Button btn_week;
+    private TextView txt_getweek;
+    private LineChart lineChart;
+    private RecyclerView rv_week;
+    /** 資料庫 **/
+    private static final String DataBaseName = "db";
+    private static final int DataBaseVersion = 9;
+    private static String DataBaseTable = "Users";
+    private static SQLiteDatabase DB;
+    private SqlDataBaseHelper sqlDataBaseHelper;
+    /** RecyclerView **/
+    MyListAdapter myListAdapter;
+    ArrayList<String> Heart = new ArrayList<>();
+    ArrayList<String> Date = new ArrayList<>();
+    ArrayList<String> state = new ArrayList<>();
+    /** itemClick **/
+    private PopupWindow record;
+    private TextView txt_lf,txt_sdnn,txt_hf;
+    private Button record_ok;
+    public static int lastPosition = 0;
+    private ViewPager vp_individual;
+    private TabLayout tl_individual;
+    private ResultPagerAdapter adapter;
+    /** popupWindow **/
+    private PopupWindow weekset;
+    private NumberPicker np_year,np_month,np_week;
+    private Button week_ok;
+    private Date date = new Date();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,7 +82,204 @@ public class RecordWeek extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_record_week, container, false);
+        view = inflater.inflate(R.layout.fragment_record_week, container, false);
+        btn_week = view.findViewById(R.id.btn_week);
+        txt_getweek = view.findViewById(R.id.txt_getweek);
+        btn_week.setOnClickListener(lis);
+        initRecycler();
+        setWeek();
+        return view;
+    }
+
+    public void initRecycler(){
+        // 建立SQLiteOpenHelper物件
+        sqlDataBaseHelper = new SqlDataBaseHelper(getActivity(),DataBaseName,null,DataBaseVersion,DataBaseTable);
+        DB = sqlDataBaseHelper.getWritableDatabase(); // 開啟資料庫
+        Cursor D = DB.rawQuery("SELECT * FROM Data",null);
+        D.moveToFirst();
+
+        rv_week = view.findViewById(R.id.rv_week);
+        rv_week.setLayoutManager(new LinearLayoutManager(getActivity()));
+        rv_week.addItemDecoration(new DividerItemDecoration(getActivity(),DividerItemDecoration.VERTICAL));
+        myListAdapter = new MyListAdapter();
+        rv_week.setAdapter(myListAdapter);
+        //HashMap<String,String> hashMap = new HashMap<>();
+        for (int i = 0;i<D.getCount();i++){     //按照順序顯示資料
+            Heart.add(D.getString(8));
+            Date.add(D.getString(1));
+            state.add(D.getString(2));
+            D.moveToNext();     //下一筆資料
+        }
+    }
+
+    public void setWeek(){
+        View weekView = LayoutInflater.from(getActivity()).inflate(R.layout.popwindow_week,null);
+        weekset = new PopupWindow(weekView);
+        weekset.setWidth(ViewGroup.LayoutParams.WRAP_CONTENT);
+        weekset.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
+        weekset.setFocusable(false);
+        week_ok = weekView.findViewById(R.id.week_ok);
+        np_year = weekView.findViewById(R.id.np_year);
+        np_month = weekView.findViewById(R.id.np_month);
+        np_week = weekView.findViewById(R.id.np_week);
+        npSet();
+        week_ok.setOnClickListener(lis);
+    }
+
+    public void npSet(){
+        Calendar cal = Calendar.getInstance();
+        Calendar calendar = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        np_year.setMinValue(year-10);
+        np_year.setMaxValue(year+10);
+        np_year.setValue(year);
+        np_month.setMinValue(1);
+        np_month.setMaxValue(12);
+        np_month.setValue(cal.get(Calendar.MONTH));
+        int month = np_month.getValue();
+        int day = cal.getActualMaximum(month);
+        calendar.set(Calendar.DAY_OF_MONTH,day);
+        int week = calendar.get(Calendar.WEEK_OF_MONTH);
+        np_week.setMinValue(1);
+        np_week.setMaxValue(week);
+        np_week.setValue(cal.get(Calendar.WEEK_OF_MONTH));
+    }
+
+    View.OnClickListener lis = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()){
+                case R.id.record_ok:{
+                    record.dismiss();
+                    break;
+                }
+                case R.id.btn_week:{
+                    //Toast.makeText(getActivity(),"0.0.0",Toast.LENGTH_SHORT).show();
+                    weekset.showAtLocation(view, Gravity.CENTER_HORIZONTAL, 0, 0);
+                    break;
+                }
+                case R.id.week_ok:{
+                    weekset.dismiss();
+                    break;
+                }
+            }
+        }
+    };
+
+    private class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.ViewHolder>{
+
+        class ViewHolder extends RecyclerView.ViewHolder{
+            private TextView item_heartrate,item_status,item_date;
+
+            public ViewHolder(@NonNull View itemView) {
+                super(itemView);
+                item_heartrate = itemView.findViewById(R.id.item_heartrate);
+                item_status = itemView.findViewById(R.id.item_status);
+                item_date = itemView.findViewById(R.id.item_date);
+                popRecord();
+
+                itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        Note.Date = item_date.getText().toString();
+                        Log.d("date",""+Note.Date);
+
+                        // 建立SQLiteOpenHelper物件
+                        sqlDataBaseHelper = new SqlDataBaseHelper(getActivity(),DataBaseName,null,DataBaseVersion,DataBaseTable);
+                        DB = sqlDataBaseHelper.getWritableDatabase(); // 開啟資料庫
+                        Cursor D = DB.rawQuery("SELECT * FROM Data WHERE time LIKE '"+Note.Date+"'",null);
+                        D.moveToFirst();
+                        //點擊歷史紀錄顯示的值
+                        txt_lf.setText(D.getString(6));
+                        txt_hf.setText(D.getString(7));
+                        txt_sdnn.setText(D.getString(5));
+                        //太極圖數據
+                        Note.HFn = D.getDouble(7);
+                        Note.LFn = D.getDouble(6);
+                        Note.sdNN = D.getDouble(4);
+                        //散點圖數據
+                        Note.RRi.clear();   //先清空怕資料重疊
+                        String test = D.getString(9).replaceAll(" ","");    //取得數據以及清除當中的空格
+                        Log.d("test",""+test);
+                        String[] sqlrri = test.split("\\[|,|\\]",0);    //去除括弧和逗號
+                        Log.d("sqlrri",""+ Arrays.toString(sqlrri));
+                        for (int i=1;i<sqlrri.length;i++){
+                            Note.RRi.add(Integer.parseInt(sqlrri[i]));      //轉成int並寫到Note.RRi當變數用
+                        }
+                        Log.d("Note.RRi",""+Note.RRi);
+
+                        record.showAtLocation(view, Gravity.CENTER_HORIZONTAL, 0, 0);
+                    }
+                });
+            }
+        }
+
+        @NonNull
+        @Override
+        public MyListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_record,parent,false);
+            return new MyListAdapter.ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MyListAdapter.ViewHolder holder, int position) {
+            holder.item_heartrate.setText(Heart.get(position));
+            holder.item_status.setText(state.get(position));
+            holder.item_date.setText(Date.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return Date.size();
+        }
+
+        public void popRecord(){
+            View view = LayoutInflater.from(getActivity()).inflate(R.layout.popwindow_record,null);
+            record = new PopupWindow(view);
+            int width = getActivity().getWindowManager().getDefaultDisplay().getWidth();
+            record.setWidth(width*5/7);
+            record.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
+            record.setFocusable(false);
+            txt_lf = view.findViewById(R.id.txt_lf);
+            txt_sdnn = view.findViewById(R.id.txt_sdnn);
+            txt_hf = view.findViewById(R.id.txt_hf);
+            record_ok = view.findViewById(R.id.record_ok);
+            vp_individual = view.findViewById(R.id.vp_individual);
+            tl_individual = view.findViewById(R.id.tl_individual);
+            record_ok.setOnClickListener(lis);
+            //initView();
+        }
+
+        public void initView(){
+            vp_individual.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+                @Override
+                public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+                }
+
+                @Override
+                public void onPageSelected(int position) {
+                    lastPosition = position;
+                }
+
+                @Override
+                public void onPageScrollStateChanged(int state) {
+
+                }
+            });
+            setupViewPager(vp_individual);
+            tl_individual.setupWithViewPager(vp_individual);
+            tl_individual.getTabAt(0).setIcon(R.drawable.scatter_graph);
+            tl_individual.getTabAt(1).setIcon(R.drawable.yin_yang);
+        }
+
+        private void setupViewPager(ViewPager viewPager){
+            List<Fragment> fragmentList = new ArrayList<>();
+            fragmentList.add(new ScatterFragment());
+            fragmentList.add(new TaijiFragment());
+            adapter = new ResultPagerAdapter(getActivity().getSupportFragmentManager(),getActivity(),fragmentList);
+            viewPager.setAdapter(adapter);
+        }
     }
 }
